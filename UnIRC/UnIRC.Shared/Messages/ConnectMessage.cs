e88c7ce2ba6a18962e6e47281f614980c2d72bd6
Ash Wolford <piro@live.com>
2015-11-20 04:34:20 -0600

    ConnectMessage not added for whatever reason

---
+++ UnIRC/UnIRC.Shared/Messages/ConnectMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnIRC.Models;
+
+namespace UnIRC.Shared.Messages
+{
+    public class ConnectMessage : Message
+    {
+        public Network Network { get; set; }
+        public Server Server { get; set; }
+
+        // ReSharper disable ExplicitCallerInfoArgument
+        public ConnectMessage(Network network, Server server,
+            [CallerFilePath] string callerFilePath = null,
+            [CallerMemberName] string callerMemberName = null,
+            [CallerLineNumber] int callerLineNumber = -1)
+            : base("Connect", $@"User initiated connection to '{network?.Name}' via {server?.DisplayName}",
+                  callerFilePath, callerMemberName, callerLineNumber)
+        {
+            Network = network;
+            Server = server;
+        }
+    }
+}
