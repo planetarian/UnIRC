8381bb1d9481b4c937cba74cd729b2a7ff9639cd
Ash Wolford <piro@live.com>
2015-11-20 15:33:37 -0600

    UI tweaks

--- UnIRC/UnIRC.Universal/Views/NetworksView.xaml.cs
+++ UnIRC/UnIRC.Universal/Views/NetworksView.xaml.cs
@@ -1,35 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Windows.UI.Xaml.Controls;
 
 namespace UnIRC.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class NetworksView : Page
     {
         public NetworksView()
         {
-            this.InitializeComponent();
-        }
-
-        private void NewServerClick(object sender, RoutedEventArgs e)
-        {
-
+            InitializeComponent();
         }
     }
 }
